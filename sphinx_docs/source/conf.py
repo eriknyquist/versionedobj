5ae4a5343667ae296cd3162476436a710326800d
Erik Nyquist <eknyquist@gmail.com>
2022-09-15 20:30:35 -0700

    Bump version, update docs build

--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -22,7 +22,7 @@ project = 'versionedobj'
 copyright = '2022, Erik K. Nyquist'
 author = 'Erik K. Nyquist'
 
-version = 'v0.2.4'
+version = 'v0.2.5'
 
 # The full version, including alpha/beta/rc tags
 release = version
--- versionedobj/__init__.py
+++ versionedobj/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 from versionedobj.object import VersionedObject, CustomValue
 from versionedobj.exceptions import LoadObjError, InvalidFilterError, InputValidationError
