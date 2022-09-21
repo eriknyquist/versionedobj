9448976976614a8f56df3875bc239aa1966e813f
Erik Nyquist <eknyquist@gmail.com>
2022-09-20 21:08:43 -0700

    setup.py: add more classifiers/keywords

--- setup.py
+++ setup.py
@@ -8,18 +8,6 @@ from versionedobj import __version__
 HERE = os.path.abspath(os.path.dirname(__file__))
 README = os.path.join(HERE, "README.rst")
 
-classifiers = [
-    'License :: OSI Approved :: Apache Software License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Natural Language :: English',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'Intended Audience :: Education',
-    'Intended Audience :: Information Technology',
-]
-
 class RunVersionedObjTests(Command):
     user_options = []
 
@@ -47,8 +35,36 @@ setup(
     author_email='eknyquist@gmail.com',
     license='Apache 2.0',
     packages=['versionedobj'],
-    classifiers = classifiers,
     cmdclass={'test': RunVersionedObjTests},
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    python_requires=">=3.7",
+    keywords=[
+        "serialization",
+        "json",
+        "api",
+        "marshal",
+        "marshalling",
+        "deserialization",
+        "validation",
+        "schema"
+    ],
+    classifiers=[
+        "Classifier: Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    project_urls={
+        "Documentation": "https://eriknyquist.github.io/versionedobj",
+        "Issues": "https://github.com/eriknyquist/versionedobj/issues",
+        "Contributions": "https://eriknyquist.github.io/versionedobj/#contributions"
+    }
 )
