8ad5b02fd7f3460cc28af8d259fd156eff2e9941
Erik Nyquist <eknyquist@gmail.com>
2022-09-21 23:25:22 -0700

    Bump version, update docs build

--- versionedobj/__init__.py
+++ versionedobj/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 from versionedobj.object import VersionedObject, CustomValue, migration
 from versionedobj.exceptions import LoadObjError, InvalidFilterError, InputValidationError, InvalidVersionAttributeError
