62c4ea7ec8e74b17dc42b530fe7bd84add23e4a4
Erik Nyquist <eknyquist@gmail.com>
2022-09-21 23:28:57 -0700

    setup.py: fix classifiers

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         "schema"
     ],
     classifiers=[
-        "Classifier: Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
